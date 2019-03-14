80629af1ead73dbe1444fbc31cc8a1ccf974735b
ossset <ossset@protonmail.com>
2019-03-14 12:23:13 +0300

    feat: add triagnle counting function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let x = preferences.length; 
+  let y = 0;  
+  for (let i = 0; i<x; i++) { 
+      let a = preferences[i] - 1;
+      if (a == i) continue;
+      let b = preferences[a] - 1;
+      if (b == a) continue;
+      let c = preferences[b] - 1;
+      if (b ==c) continue;
+      if (c == i) {
+          y++;
+      }
+   }
+   return y/3;
 };
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
